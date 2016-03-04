5f45c961cda01ed6878917ffbd208842b17f53ad
aamulumi <flokauder@hotmail.com>
2016-03-04 16:48:12 +0100

    fix(front): Show all generated files on the page

---
+++ client/components/generatedFile/generatedFiles.js
@@ -0,0 +1,28 @@
+Template.generatedFiles.helpers({
+  'files': function() {
+    if (Session.get('generatedFiles') === undefined)
+      return [];
+      
+    let files = [];
+
+    for (let f of Session.get('generatedFiles')){
+      files.push(Collections.PDF.findOne(f));
+    }
+
+    return files;
+  }
+});
+
+Template.generatedFiles.events({
+  'click .generatedPDFLink': function(event) {
+    event.preventDefault();
+
+    let url = 'data:application/pdf;base64,';
+    Meteor.call('PDF.get',
+      event.target.name, (err, res) => {
+        console.log(err);
+        window.open(url + res);
+      });
+
+  }
+});
