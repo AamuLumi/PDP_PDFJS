dbccaa081cd6fa9cdaf5c1b2b8cf6664e8cee4d8
valcol <valcol.nc@gmail.com>
2016-03-29 07:08:37 +0200

    Fix helper

--- client/components/form/form.js
+++ client/components/form/form.js
@@ -6,7 +6,7 @@ if (Meteor.isClient) {
   Template.form.helpers({
 
     isTemplateSelected: function(){
-      return (Session.get('formSchema') != '');
+      return (Session.get('selectedTemplate') != '');
     },
 
     formSchema: function() {
